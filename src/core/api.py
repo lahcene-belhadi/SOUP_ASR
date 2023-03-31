e73e6b5927bd0d666479e7a292b6e0297fb2c22b
Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
2023-03-31 07:07:28 +0200

    Adds /transcribe route

--- src/core/api.py
+++ src/core/api.py
@@ -4,11 +4,22 @@
 #
 # Authors: Lahcène Belhadi <lahcene.belhadi@alumni.univ-avignon.fr>
 
-from flask import Flask
+from flask import Flask, request
 
 api = Flask(__name__)
 
 @api.get("/")
 def root() -> str:
+    """Displays an \"Hello world!\""""
     return "Hello world!"
+
+@api.post("/transcribe")
+def transcribe() -> str:
+    """Enables user to pass an audio file and get the text from their speech"""
+    audio_file = request.files.get("audio_file")
+
+    if audio_file is None:
+        return "No file provided!"
+
+    return "Got the file!"
 
